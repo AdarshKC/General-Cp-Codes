3017c08c1f420835edb3d24f3d399be89de1a836
Adarsh Chaudhary <adarshrohit217@gmail.com>
2019-04-20 19:05:55 +0530

    Create 520A.cpp

---
+++ codeforces/520A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define fr(i,a,b) for(ll i=a; i<b; i++)
+#define fr1(i,a,b) for(ll i=a; i>=b; i--)
+#define ll int
+#define ld long double
+#define vi vector<ll>
+#define pb push_back
+#define mp(i,j) make_pair(i,j)
+#define fi first
+#define se second
+#define input(a,n) fr(i,0,n)cin>>a[i]
+#define output(a,n) fr(i,0,n)cout<<a[i]<<" "
+#define dbg cout<<"hurr"<<endl;
+#define dbg2 cout<<"hurr2"<<endl;
+#define md 1000000007
+#define sp << " " <<
+#define pi 3.14159265358979323846
+#define fixd(x) cout << fixed << setprecision(x);
+
+int main()
+{
+	ll n;
+	cin >> n;
+	string s;
+	cin >> s;
+	set <char> ss;
+	fr(i,0,n)
+	{
+		char c=tolower(s[i]);
+		ss.insert(c);
+	}
+	if(ss.size()==26)
+		cout << "YES";
+	else
+		cout << "NO";
+}
