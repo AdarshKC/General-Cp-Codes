6d18d2574ed6de38353e47546f1d311be2b45aa7
Adarsh Chaudhary <adarshrohit217@gmail.com>
2019-06-15 15:03:47 +0530

    Create 484A.cpp

---
+++ codeforces/484A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define fr(i,a,b) for(ll i=a; i<b; i++)
+#define fr1(i,a,b) for(ll i=a; i>=b; i--)
+#define ll long long int
+#define ld long double
+#define vi vector<ll>
+#define vii vector <pair<ll,ll> >
+#define viii vector <pair<ll,pair<ll,ll> > >
+#define pb push_back
+#define all(v) (v).begin(),(v).end()
+#define mp(i,j) make_pair(i,j)
+#define fi first
+#define se second
+#define input(a,n) fr(i,0,n)cin>>a[i]
+#define output(a,n) fr(i,0,n)cout<<a[i]<<" "
+#define dbg cout<<"hurr"<<endl;
+#define dbg2 cout<<"hurr2"<<endl;
+#define md 1000000007
+#define inf 999999999999999999
+#define sp << " " <<
+#define pi 3.14159265358979323846
+#define fixd(x) cout << fixed << setprecision(x);
+#define sz size()
+
+int main()
+{
+	fast;
+	ll n,l,r;
+	cin >> n;
+	while(n--)
+	{
+		cin >> l >> r;
+		fr(i,0,63)
+		{
+			ll x=(1LL<<i);
+			if(!(x&l) && (x|l)<=r)
+				l|=x;
+		}
+		cout << l << endl;
+	}
+}
