a3830bb6ca0a2f6f164f4e2324ae8b90cc87d142
Adarsh Chaudhary <adarshrohit217@gmail.com>
2019-06-18 23:27:16 +0530

    Create 407B.cpp

---
+++ 407B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define fr(i,a,b) for(ll i=a; i<b; i++)
+#define fr1(i,a,b) for(ll i=a; i>=b; i--)
+#define ll long long int
+#define ld long double
+#define vi vector<ll>
+#define vii vector <pair<ll,ll> >
+#define viii vector <pair<ll,pair<ll,ll> > >
+#define pb push_back
+#define all(v) (v).begin(),(v).end()
+#define mp(i,j) make_pair(i,j)
+#define fi first
+#define se second
+#define input(a,n) fr(i,0,n)cin>>a[i]
+#define output(a,n) fr(i,0,n)cout<<a[i]<<" "
+#define dbg cout<<"hurr"<<endl;
+#define dbg2 cout<<"hurr2"<<endl;
+#define md 1000000007
+#define inf 999999999999999999
+#define sp << " " <<
+#define pi 3.14159265358979323846
+#define fixd(x) cout << fixed << setprecision(x);
+#define sz size()
+
+int main()
+{
+	fast;
+	ll n;
+	cin >> n;
+	ll a[n],dp[n+1]={0};
+	input(a,n);
+	dp[0]=0;
+	fr(i,1,n+1)
+		dp[i]=(((2*dp[i-1])%md+2)%md+md-dp[a[i-1]-1])%md;
+	cout << dp[n];
+}
