912b8dc4d1cb8830ea2d0f0f8537aa17b726b455
Adarsh Chaudhary <adarshrohit217@gmail.com>
2019-06-21 04:31:42 +0530

    Create 388A.cpp

---
+++ codeforces/388A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define fr(i,a,b) for(ll i=a; i<b; i++)
+#define fr1(i,a,b) for(ll i=a; i>=b; i--)
+#define ll long long int
+#define ld long double
+#define vi vector<ll>
+#define vii vector <pair<ll,ll> >
+#define viii vector <pair<ll,pair<ll,ll> > >
+#define pb push_back
+#define all(v) (v).begin(),(v).end()
+#define mp(i,j) make_pair(i,j)
+#define fi first
+#define se second
+#define input(a,n) fr(i,0,n)cin>>a[i]
+#define output(a,n) fr(i,0,n)cout<<a[i]<<" "
+#define dbg cout<<"hurr"<<endl;
+#define dbg2 cout<<"hurr2"<<endl;
+#define md 1000000007
+#define inf 999999999999999999
+#define sp << " " <<
+#define pi 3.14159265358979323846
+#define fixd(x) cout << fixed << setprecision(x);
+#define sz size()
+
+int main()
+{
+	fast;
+	ll n,ans=0;
+	cin >> n;
+	ll a[n],d[n]={0};
+	input(a,n);
+	sort(a,a+n);
+	while(1)
+	{
+		ll flag=0,last=0;
+		fr(i,0,n)
+		{
+			if(!d[i] && a[i]>=last)
+			{
+				last++;
+				d[i]=1;
+				flag=1;
+			}
+		}
+		if(!flag)
+			break;
+		ans++;
+	}
+	cout << ans;
+}
